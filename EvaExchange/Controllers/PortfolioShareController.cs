cf67fe20f7802007895ec3972fea82f890ad9ccc
Tolga Ahmet Çetin <tolga-ahmet@hotmail.com>
2024-06-28 15:22:27 +0300

    Added Portfolio Controller

---
+++ EvaExchange/Controllers/PortfolioShareController.cs
@@ -0,0 +1,25 @@
+﻿using EvaExchange.Entities;
+using EvaExchange.Interfaces;
+using EvaExchange.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvaExchange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortfolioShareController : Controller
+    {
+        private readonly IPortfolioShareRepository _portfolioShareRepository;
+
+        public PortfolioShareController(IPortfolioShareRepository portfolioShareRepository)
+        {
+            _portfolioShareRepository = portfolioShareRepository;
+        }
+        [Route("GetAll")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PortfolioShare>>> GetAll()
+        {
+            return _portfolioShareRepository.GetAll();
+        }
+    }
+}
